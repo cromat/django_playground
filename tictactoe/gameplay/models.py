57162aa0389f667670e381ac0629f4efcdef5610
Tomislav Brabec <brabec.tomislav@gmail.com>
2018-02-26 12:44:23 +0100

    Moved git to parent folder. Tictactoe added static files and templates.

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Q
 
 
 GAME_STATUS_CHOICES = (
@@ -11,12 +12,25 @@ GAME_STATUS_CHOICES = (
 )
 
 
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+        return self.filter(
+            Q(first_player=user) | Q(second_player=user)
+        )
+
+    def active(self):
+        return self.filter(
+            Q(status='F') | Q(status='S')
+        )
+
+
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="games_first_player", on_delete=models.DO_NOTHING)
     second_player = models.ForeignKey(User, related_name="games_second_player", on_delete=models.DO_NOTHING)
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+    objects = GamesQuerySet.as_manager()
 
     def __str__(self):
         return "{0} vs {1}".format(self.first_player, self.second_player)
